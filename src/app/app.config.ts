744cf9eb76ef3c84e5c70d4a965224f03f67c3b0
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-09-02 13:48:37 -0700

    feat: add proxy API support for dev environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
     provideRouter(routes),
     provideAnimationsAsync(),
     providePrimeNG({
