7f183bec844dc952b29e57f93f7bc0e7b4fd7f3f
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-09-01 22:10:07 -0700

    fix: select now auto populates value

--- src/app/pages/dashboard/crud-components/crud-dialog/select/select.component.ts
+++ src/app/pages/dashboard/crud-components/crud-dialog/select/select.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { MessageModule } from 'primeng/message';
 import { SelectModule } from 'primeng/select';
@@ -6,17 +7,29 @@ import { CrudFormField } from '../crud-form-field';
 
 @Component({
   selector: 'cs-select',
-  imports: [SelectModule, FloatLabelModule, MessageModule],
+  imports: [SelectModule, FloatLabelModule, MessageModule, FormsModule],
   templateUrl: './select.component.html',
   styleUrl: './select.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SelectComponent extends CrudFormField {
+  /**
+   * The options available to select.
+   */
   options = input.required<unknown[]>();
 
+  /**
+   * The key in the option objects that uniquely identifies each option.
+   */
   dataKey = input.required<string>();
 
+  /**
+   * The key in the option objects that is used for the label.
+   */
   optionLabel = input.required<string>();
 
+  /**
+   * The key in the option objects that is used for the value.
+   */
   optionValue = input.required<string>();
 }
