19139ffe9323e4a09acf5a7d9b72377b46ec712e
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-12-26 17:14:38 -0800

    update(primeNG): v21.0.2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
@@ -12,14 +11,13 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideHttpClient(),
     provideApi({
       basePath: environment.apiUrl,
       withCredentials: true
     }),
     provideRouter(routes),
-    provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura
